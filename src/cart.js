2dfec46c9add6148da81f9d4be08908f134abcb7
michaelcodes1993 <lpskirito7@outlook.com>
2020-08-16 21:10:04 -0400

    added capital letters to names of files

--- src/cart.js
+++ src/cart.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import SummaryItem from './SummaryItem';
-import Total from './Total';
+import React from "react";
+import SummaryItem from "./SummaryItem";
+import Total from "./Total";
 
-// I HATE THIS
+//refactored names of apps
 
-export default function Cart(props){
-  return (
-  <section className="main__summary">
-    <h2>Your cart</h2>
-      {Object.keys(props.selected).map((feature, idx) => {
-        const featureHash = feature + '-' + idx;
-        const selectedOption = props.selected[feature];
-        return (
-        <SummaryItem 
-          key={featureHash}
-          name={feature}
-          cost={selectedOption.cost}
-          option={selectedOption.name}
-        />
-        )
-      })}
-  <Total 
-    total={props.total}
-    selected={props.selected}
-  />
-  </section>
-  )
+export default function Cart(props) {
+	return (
+		<section className="main__summary">
+			<h2>Your cart</h2>
+			{Object.keys(props.selected).map((feature, idx) => {
+				const featureHash = feature + "-" + idx;
+				const selectedOption = props.selected[feature];
+				return (
+					<SummaryItem
+						key={featureHash}
+						name={feature}
+						cost={selectedOption.cost}
+						option={selectedOption.name}
+					/>
+				);
+			})}
+			<Total total={props.total} selected={props.selected} />
+		</section>
+	);
 }
